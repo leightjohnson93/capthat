1c361e69f4fdb81892f4b0e792084c709ea80612
Leight Johnson <leightjohnson93@gmail.com>
2018-10-31 19:13:08 -0400

    uses fb profile pic on login

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Login from "./Login";
 import EditProfile from "./EditProfile";
 import firebase from "firebase/app";
-import axios from "axios";
+// import axios from "axios";
 import base, { firebaseApp } from "../base";
 import "../css/App.css";
 
@@ -10,18 +10,19 @@ class Profile extends Component {
   state = { editProfile: false };
 
   authHandler = async authData => {
-    const { uid, displayName, email } = authData.user;
+    const { uid, displayName, email, photoURL } = authData.user;
     this.uid = authData.user.uid;
     const users = await base.fetch("users", { context: this });
     if (!users[uid]) {
       const userRef = firebase.database().ref("users");
-      userRef.set({ [uid]: { uid, displayName, email } });
+      userRef.set({ [uid]: { uid, displayName, email, photoURL } });
     }
     this.props.history.push(`/user/${uid}`);
     base.syncState(`users/${uid}`, {
       context: this,
       state: uid
     });
+    console.log(authData);
   };
 
   authenticate = provider => {
@@ -39,34 +40,31 @@ class Profile extends Component {
     this.props.history.push(`/`);
   };
 
-  updateProfile = updatedProfile => {
+  updateProfile = updatedProfile =>
     this.setState({ [this.uid]: updatedProfile, editProfile: false });
-  };
 
   handleEdit = () => this.setState({ editProfile: true });
 
-  fetchInstagram = async handle => {
-    const userInfoSource = await axios.get(
-      `https://www.instagram.com/${handle}/`
-    );
-    const jsonObject = userInfoSource.data
-      .match(
-        /<script type="text\/javascript">window\._sharedData = (.*)<\/script>/
-      )[1]
-      .slice(0, -1);
-    const profPic = JSON.parse(jsonObject).entry_data.ProfilePage[0].graphql
-      .user.profile_pic_url_hd;
-    const uid = { ...this.state[this.uid] };
-    uid.profPic = profPic;
-    console.log(uid);
-    this.setState({ [this.uid]: uid });
-  };
+  // fetchInstagram = async handle => {
+  //   const userInfoSource = await axios.get(
+  //     `https://www.instagram.com/${handle}/`
+  //   );
+  //   const jsonObject = userInfoSource.data
+  //     .match(
+  //       /<script type="text\/javascript">window\._sharedData = (.*)<\/script>/
+  //     )[1]
+  //     .slice(0, -1);
+  //   const profPic = JSON.parse(jsonObject).entry_data.ProfilePage[0].graphql
+  //     .user.profile_pic_url_hd;
+  //   const uid = { ...this.state[this.uid] };
+  //   uid.profPic = profPic;
+  //   this.setState({ [this.uid]: uid });
+  // };
 
-  componentWillUpdate() {
-    const uid = this.state[this.uid];
-    console.log(uid);
-    if (uid && uid.handle) this.fetchInstagram(uid.handle);
-  }
+  // componentWillUpdate() {
+  //   const uid = this.state[this.uid];
+  //   if (uid && uid.handle) this.fetchInstagram(uid.handle);
+  // }
 
   render() {
     if (!this.state[this.uid]) {
@@ -77,7 +75,7 @@ class Profile extends Component {
       <div className="App">
         <header className="App-header">
           <p>{this.state[this.uid].handle}</p>
-          <img src={this.state[this.uid].profPic} alt="Profile" />
+          <img src={this.state[this.uid].photoURL} alt="Profile" />
           <button onClick={this.logout}>Logout</button>
           {this.state.editProfile || !this.state[this.uid].handle ? (
             <EditProfile
